6eb12d2004f6cceca0d27dfd0c3dab09e8a8e0f2
KshitizSachan <kshitizsachan422@gmail.com>
2023-06-11 21:54:24 +0530

    initial backend with 7 apis of auth and blog

---
+++ server/Routes/Auth.js
@@ -0,0 +1,109 @@
+const express = require('express')
+const User = require('../models/User')
+const router = express.Router()
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
+const axios = require('axios')
+const fetch = require('../middleware/fetchdetails');
+const jwtSecret = "HaHa"
+router.post('/createuser', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+    body('name').isLength({ min: 3 })
+], async (req, res) => {
+    let success = false
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ success, errors: errors.array() })
+    }
+    const salt = await bcrypt.genSalt(10)
+    let securePass = await bcrypt.hash(req.body.password, salt);
+    try {
+        await User.create({
+            name: req.body.name,
+            password: securePass,
+            email: req.body.email,
+        }).then(user => {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authToken = jwt.sign(data, jwtSecret);
+            success = true
+            res.json({ success, authToken })
+        })
+            .catch(err => {
+                console.log(err);
+                res.json({ error: "Please enter a unique value." })
+            })
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+// Authentication a User, No login Requiered
+router.post('/login', [
+    body('email', "Enter a Valid Email").isEmail(),
+    body('password', "Password cannot be blank").exists(),
+], async (req, res) => {
+    let success = false
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body;
+    try {
+        let user = await User.findOne({ email }); 
+        if (!user) {
+            return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
+        }
+
+        const pwdCompare = await bcrypt.compare(password, user.password); // this return true false.
+        if (!pwdCompare) {
+            return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        console.log(user);
+        success = true;
+        const authToken = jwt.sign(data, jwtSecret);
+        res.json({ success, authToken })
+
+
+    } catch (error) {
+        console.error(error.message)
+        res.send("Server Error")
+    }
+})
+
+// Get user details by email
+router.post('/getUserByEmail', async (req, res) => {
+    const { email } = req.body;
+  
+    try {
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      res.json({ name: user.name, email: user.email });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  });
+  
+
+
+
+
+
+
+module.exports = router
